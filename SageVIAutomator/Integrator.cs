35fd32f076be2cf777f265b5a2901d259cc8fe38
Anthony Peterson <apeterson@burnettefoods.com>
2016-06-08 14:41:14 -0400

    Implemented VI launch

---
+++ SageVIAutomator/Integrator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SageVIAutomator
+{
+    public class Integrator
+    {
+        private const string PROGRAM_FILE = "Pvxwin32.exe"; 
+
+        private string uncPath;
+        private string username;
+        private string password;
+        private string company;
+        private string jobName;
+        private string fullPath;
+
+        public Integrator(string uncPath, string username, string password, string company, string jobName)
+        {
+            this.uncPath = uncPath;
+            this.username = username;
+            this.password = password;
+            this.company = company;
+            this.jobName = jobName;
+
+            this.fullPath = uncPath + @"\" + PROGRAM_FILE;
+        }
+
+        public void runJob()
+        {
+            var processInfo = new ProcessStartInfo(fullPath);
+            processInfo.WorkingDirectory = uncPath;
+            processInfo.Arguments = string.Format("..launcher sota.ini ..SOA Startup.M4P -ARG DIRECT UION {0} {1} {2} {3} AUTO", username, password, company, jobName);
+
+            Process.Start(processInfo);
+        }
+    }
+}
