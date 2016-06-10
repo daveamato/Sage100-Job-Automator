b548267af8b3ed5838668482c784f4a42af6bb9e
Anthony Peterson <apeterson@burnettefoods.com>
2016-06-10 10:58:27 -0400

    Added parameter for AUTO/MANUAL VI launch

--- SageVIAutomator/Integrator.cs
+++ SageVIAutomator/Integrator.cs
@@ -18,14 +18,17 @@ namespace SageVIAutomator
         private string company;
         private string jobName;
         private string fullPath;
+        private string automan;
 
-        public Integrator(string uncPath, string username, string password, string company, string jobName)
+        public Integrator(string uncPath, string username, string password, string company, string jobName, bool isAuto = true)
         {
             this.uncPath = uncPath;
             this.username = username;
             this.password = password;
             this.company = company;
             this.jobName = jobName;
+
+            this.automan = (isAuto ? "AUTO" : "MANUAL");
 
             this.fullPath = uncPath + @"\" + PROGRAM_FILE;
         }
@@ -34,7 +37,7 @@ namespace SageVIAutomator
         {
             var processInfo = new ProcessStartInfo(fullPath);
             processInfo.WorkingDirectory = uncPath;
-            processInfo.Arguments = string.Format("..launcher sota.ini ..SOA Startup.M4P -ARG DIRECT UION {0} {1} {2} {3} AUTO", username, password, company, jobName);
+            processInfo.Arguments = string.Format("..launcher sota.ini ..SOA Startup.M4P -ARG DIRECT UION {0} {1} {2} {3} {4}", username, password, company, jobName, automan);
 
             return Process.Start(processInfo);
         }
